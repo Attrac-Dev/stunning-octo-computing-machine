901a3142005bf08a5b0f8350ab601673ea429961
Skyler Saville <skylersaville@gmail.com>
2023-04-09 13:48:53 -0600

    Fixing the form to not redirect on submit

--- components/NewsLetterSignup.vue
+++ components/NewsLetterSignup.vue
@@ -1,81 +1,64 @@
 <template>
-    <div>
-      <!-- Show form or confirmation message based on formSubmitted state -->
-      <form 
-        name="newsletter-signup"
-        method="POST"
-        netlify-honeypot="bot-field"
-        data-netlify="true"
-        v-if="!formSubmitted"   
-        @submit.prevent="submitForm"
-      >
-        <input name="bot-field" class="hidden">
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter email address"
-          v-model="email"
-        />
-        <input
-          type="submit"
-          value="Subscribe"
-        />
-        <p v-if="formSubmitted && !isValidEmail()" class="error-message">
-          Please enter a valid email address.
-        </p>
-      </form>
-      <div v-else>
-        <!-- Confirmation message markup here -->
-        <p>Thank you for subscribing!</p>
-      </div>
+  <div>
+    <!-- Show form or confirmation message based on formSubmitted state -->
+    <form 
+      name="newsletter-signup"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      v-if="!formSubmitted"   
+      @submit.prevent="submitForm"
+    >
+      <input name="bot-field" class="hidden">
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter email address"
+        v-model="email"
+      />
+      <input
+        type="submit"
+        value="Subscribe"
+      />
+      <p v-if="formSubmitted && !isValidEmail()" class="error-message">
+        Please enter a valid email address.
+      </p>
+    </form>
+    <div v-else>
+      <!-- Confirmation message markup here -->
+      <p>Thank you for subscribing!</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        formSubmitted: false, // Track form submission state
-        email: "", // Other data properties for form fields, e.g. email
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formSubmitted: false, // Track form submission state
+      email: "", // Other data properties for form fields, e.g. email
+    };
+  },
+  methods: {
+    isValidEmail() {
+      // You can add your own validation logic here
+      // This is a simple example that checks for a basic email format
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email);
     },
-    methods: {
-      isValidEmail() {
-        // You can add your own validation logic here
-        // This is a simple example that checks for a basic email format
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email);
-      },
-      submitForm() {
-        if (this.isValidEmail()) {
-          // Success: Email is valid, implement your subscribe logic here
-          console.log("Subscribed with email:", this.email)
-          // Prepare form data for submission
-          const formData = new FormData();
-          formData.append("email", this.email)
-          console.log(`formData== ${formData}`)
-          // Use Fetch API for form submission
-          fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: formData
-          })
-            .then(() => {
-              console.log("Form successfully submitted")
-              this.formSubmitted = true; // Update form submission state
-            })
-            .catch((error) => {
-              console.error("Error submitting form:", error)
-              this.formSubmitted = false; // Set formSubmitted to false to show error message
-            })
-        } else {
-          // Error: Invalid email, show error message
-          console.error("Invalid email:", this.email)
-          this.formSubmitted = false; // Set formSubmitted to false to show the error message
-        }
-      },
+    submitForm() {
+      if (this.isValidEmail()) {
+        // Success: Email is valid, implement your subscribe logic here
+        console.log("Subscribed with email:", this.email);
+        this.formSubmitted = true;
+      } else {
+        // Error: Invalid email, show error message
+        console.error("Invalid email:", this.email);
+        this.formSubmitted = false; // Set formSubmitted to false to show the error message
+      }
     },
-  };
-  </script>
+  },
+};
+</script>
   
   <style scoped>
   form {
