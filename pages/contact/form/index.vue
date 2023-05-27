692929fd58ef3d779126dc1a6e3520281ab4737e
Skyler Saville <skylersaville@gmail.com>
2023-05-27 10:35:29 -0600

    still working on dev form

--- pages/contact/form/index.vue
+++ pages/contact/form/index.vue
@@ -1,27 +1,25 @@
 <template>
         <div v-if="isDevelopment" class="contact-form">
-        <form name="contact" method="POST" data-netlify="true">
-            <p>
-                <label>Your Name: <input type="text" name="name" required /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" required /></label>
-            </p>
-            <p>
-                <label>Services: 
-                    <select name="services[]" v-model="service" multiple>
-                        <option disabled value="" class="option-list">Please select services</option>
-                        <option v-for="option in services" :key="option" :value="option" class="option-list">{{ option }}</option>
-                    </select>
-                </label>
-            </p>
-            <p>
-            <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-            <button type="submit">Send</button>
-            </p>
-        </form>
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Your Role: <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>     
     </div>
 
 
@@ -110,7 +108,7 @@
           'Emails',
           'Branding'
         ],
-        service: [],
+        service: '',
         submissionError: '',
         bypass: false,
       };
